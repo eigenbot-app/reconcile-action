ca405b632e577ba30a37b059f5df13fb7367546a
Joe <joe@complex64.com>
2023-01-27 17:20:18 +0100

    do-nothing action

---
+++ src/main.ts
@@ -0,0 +1,29 @@
+import * as core from "@actions/core";
+
+void run();
+
+async function run(): Promise<void> {
+  runReconcile()
+    .then((res) => {
+      if (res instanceof Error) {
+        core.setFailed(res.message);
+      }
+    })
+    .catch((err) => {
+      core.setFailed(`internal error: ${err.message}`);
+    });
+}
+
+async function runReconcile(): Promise<null | Error> {
+  const secret = core.getInput("secret");
+  if (secret === "") {
+    return Error("input `secret` was not provided");
+  }
+
+  const token = core.getInput("token");
+  if (token === "") {
+    return Error("input `token` was not provided");
+  }
+
+  return null;
+}
